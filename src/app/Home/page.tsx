4cff2824c537b7a3c50d12d6aa40508013bfef34
Hitesh0424 <hiteshpatil0424@gmail.com>
2024-03-10 18:08:14 +0530

    updated components

---
+++ src/app/Home/page.tsx
@@ -0,0 +1,14 @@
+import { NavbarDemo } from '@/components/Navbar'
+import React from 'react'
+
+
+const index = () => {
+  return (
+    <div>
+    <NavbarDemo/>
+    <div className='text-center text-cyan-200'>Home page</div>
+    </div>
+  )
+}
+
+export default index
---
+++ src/app/footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import navbar from '../navbar/navbar'
+
+const footer = () => {
+  return (
+
+     
+    <div>footer</div>
+  )
+}
+
+export default footer
